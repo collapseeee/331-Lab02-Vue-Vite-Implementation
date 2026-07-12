a49113c86993d0bba6d23fef106454a9e03ecfd7
COLLAPse <4174tt@gmail.com>
2026-07-12 22:33:41 +0700

    task 8.4: array of event

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,20 +1,21 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+// import { ref } from 'vue'
+import type { Event } from '@/types'
 
-// defineProps<{
-//   msg: string
-// }>()
-const event = ref({
-  id: 5928101,
-  category: 'animal welfare',
-  title: 'Cat Adoption Day',
-  description: 'Find your new feline friend at this event.',
-  location: 'Meow Town',
-  date: 'January 28, 2026',
-  time: '12:00',
-  petsAllowed: true,
-  organizer: 'Kat Laydee',
-})
+defineProps<{
+  event: Event
+}>()
+// const event = ref({
+//   id: 5928101,
+//   category: 'animal welfare',
+//   title: 'Cat Adoption Day',
+//   description: 'Find your new feline friend at this event.',
+//   location: 'Meow Town',
+//   date: 'January 28, 2026',
+//   time: '12:00',
+//   petsAllowed: true,
+//   organizer: 'Kat Laydee',
+// })
 </script>
 
 <template>
